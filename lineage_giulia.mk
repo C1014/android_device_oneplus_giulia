87744e9d8889cec0611c29bdd9ed02e8c6560a2b
ZeroSquared <raphaelquimno@gmail.com>
2026-06-27 20:56:18 +0800

    giulia: Update proprietary lists and blobs from lunaris

---
+++ lineage_giulia.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from giulia device
+$(call inherit-product, device/oneplus/giulia/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Lunaris Flags
+LUNARIS_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_CUSTOM_UDFPS := true
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+WITH_GMS := true
+WITH_BCR := true
+
+PRODUCT_NAME := lineage_giulia
+PRODUCT_DEVICE := giulia
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2691
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1780929007378 release-keys" \
+    BuildFingerprint=OnePlus/CPH2691IN/OP5D3BL1:16/UKQ1.231108.001/V.R4T3.37e288c-13a765a-13fa038:user/release-keys \
+    DeviceName=OP5D3BL1 \
+    DeviceProduct=CPH2691 \
+    SystemDevice=OP5D3BL1 \
+    SystemName=CPH2691
